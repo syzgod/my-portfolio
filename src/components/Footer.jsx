ead25b9fb9285ea27371eb52e9561d9e239b3087
syzgod <viktor.berczeli@gmail.com>
2023-05-01 17:50:47 +0200

    Update UI

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className='flex flex-col md:flex-row justify-center items-center text-gray-300 p-4'>
-      <p>Made with ❤️ and React JS - Tailwind CSS</p>
+      <p>Made with ❤️ and React JS - Tailwind CSS |</p>
       <a
         href='https://storyset.com/people'
         className='ml-2 border-b border-pink-600'
